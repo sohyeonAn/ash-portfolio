5cab06bd03661d35b4e6e3f01c3ed5837765c6f3
sohyeonAn <ansh4393@gmail.com>
2022-04-07 14:09:41 +0900

    💄 한글폰트 변경

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -13,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     overflow-x: hidden;
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Hahmlet', sans-serif;
   }
 `;
 
