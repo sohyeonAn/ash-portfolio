d3cd22a00a26c07f4767e8139b98cbe089b135a0
sohyeonAn <ansh4393@gmail.com>
2022-04-08 03:21:58 +0900

    ✨ work 스크롤 이벤트 추기, workItem 컴포넌트 생성

---
+++ src/components/Work/WorkItem.js
@@ -0,0 +1,104 @@
+import React from "react";
+import styled from "styled-components";
+import { ImGithub } from "react-icons/im";
+
+const Box = styled.li`
+  width: 16rem;
+  height: 40vh;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  padding: 1.5rem 2rem;
+  margin-right: 8rem;
+
+  border-radius: 0 50px 0 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid ${(props) => props.theme.body};
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    border: 1px solid ${(props) => props.theme.text};
+  }
+`;
+
+const Title = styled.h2`
+  font-size: calc(1em + 0.5vw);
+`;
+
+const Description = styled.h2`
+  font-size: calc(0.8em + 0.3vw);
+  font-weight: 500;
+`;
+
+const Tags = styled.div`
+  border-top: 2px solid ${(props) => props.theme.body};
+  padding-top: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  ${Box}:hover & {
+    border-top: 2px solid ${(props) => props.theme.text};
+  }
+`;
+const Tag = styled.span`
+  margin-right: 1rem;
+  font-size: calc(0.8em + 0.3vw);
+`;
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    cursor: pointer;
+  }
+  a:first-child {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    text-decoration: none;
+    padding: 0.5rem calc(2rem + 2vw);
+    border-radius: 0 0 0 50px;
+    font-size: calc(1em + 0.5vw);
+
+    ${Box}:hover & {
+      background-color: ${(props) => props.theme.text};
+      color: ${(props) => props.theme.body};
+    }
+  }
+
+  a:last-child {
+    color: ${(props) => props.theme.body};
+    text-decoration: none;
+    svg {
+      width: 30px;
+      height: 30px;
+    }
+
+    ${Box}:hover & {
+      & > * {
+        color: ${(props) => props.theme.text};
+      }
+    }
+  }
+`;
+const WorkItem = ({ data }) => {
+  const { _id, title, description, tags, projectLink, codeLink } = data;
+  return (
+    <Box key={_id}>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <Tags>{tags && tags.map((t, idx) => <Tag key={idx}>#{t}</Tag>)}</Tags>
+      <Footer>
+        <a href={projectLink} target="_blank" rel="noreferrer">
+          Visit
+        </a>
+        <a href={codeLink} target="_blank" rel="noreferrer">
+          <ImGithub />
+        </a>
+      </Footer>
+    </Box>
+  );
+};
+
+export default WorkItem;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import styled, { ThemeProvider } from "styled-components";
 
+import { client } from "../client";
+import { HomeButton, Logo, SocialIcons } from "../components/commons";
+import { Dog } from "../components/commons/AllSvgs";
+import { darkTheme } from "../components/Themes";
+import WorkItem from "../components/Work/WorkItem";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  min-height: 100vh;
+  height: ${(props) => `${props.length * 50}vh`};
+  position: relative;
+  overflow: hidden;
+`;
+
+const Main = styled.ul`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+
+  color: #fff;
+`;
+
+const Rotate = styled.span`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+`;
 const Work = () => {
-  return <div>Work</div>;
+  const [works, setWorks] = useState([]);
+  const ref = useRef(null);
+  const dog = useRef(null);
+
+  useEffect(() => {
+    const query = '*[_type == "project"]';
+    client.fetch(query).then((data) => setWorks(data));
+
+    let element = ref.current;
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.scrollY}px)`;
+      dog.current.style.transform = `rotate(${window.scrollY}deg)`;
+    };
+    window.addEventListener("scroll", rotate);
+    return () => window.removeEventListener("scroll", rotate);
+  }, []);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box length={works.length}>
+        <Logo theme="dark" />
+        <SocialIcons theme="dark" />
+        <HomeButton />
+        <Main ref={ref}>
+          {works &&
+            works.map((work) => <WorkItem key={work._id} data={work} />)}
+        </Main>
+        <Rotate ref={dog}>
+          <Dog width={80} height={80} fill={darkTheme.text} />
+        </Rotate>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Work;
