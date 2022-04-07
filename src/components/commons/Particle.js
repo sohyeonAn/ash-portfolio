cdbb87b4414f223494e62b7a3c11b9218c14981f
sohyeonAn <ansh4393@gmail.com>
2022-04-07 23:22:18 +0900

    🎨 skills 페이지 skill 아이템 추가

---
+++ src/components/commons/Particle.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Particles from "react-tsparticles";
+
+import lightParticle from "../../config/circle-particle-light-config.json";
+import darkParticle from "../../config/circle-particle-dark-config.json";
+const Box = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 0;
+`;
+const Particle = (props) => {
+  return (
+    <Box>
+      <Particles
+        style={{ position: "absolute", top: 0 }}
+        options={props.theme === "light" ? lightParticle : darkParticle}
+      />
+      );
+    </Box>
+  );
+};
+
+export default Particle;
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,7 +1,154 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+
+import { client, urlFor } from "../client";
+import { HomeButton, Logo, SocialIcons } from "../components/commons";
+import Particle from "../components/commons/Particle";
+import { lightTheme } from "../components/Themes";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  min-height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
+
+const Section = styled.section`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  h3 {
+    font-size: calc(1em + 1vw);
+    margin-bottom: 1rem;
+  }
+  ${Main}:hover & {
+    & > * {
+    }
+  }
+`;
+const IconBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  img {
+    width: calc(3em + 2vw);
+    height: calc(3em + 2vw);
+  }
+  span {
+    text-align: center;
+    font-size: 0.9rem;
+  }
+`;
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "skills"]';
+    client.fetch(query).then((data) => setSkills(data));
+  }, []);
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <Logo />
+        <SocialIcons theme="light" />
+        <HomeButton theme="light" />
+        <Particle theme="light" />
+        <Main>
+          <Section>
+            <h3>Frontend</h3>
+            <IconBox>
+              {skills
+                .filter((skill) => skill.type === "frontend")
+                .map((skill) => (
+                  <Icon key={skill.name}>
+                    <img src={urlFor(skill.icon)} alt={skill.name} />
+                    <span>{skill.name}</span>
+                  </Icon>
+                ))}
+            </IconBox>
+          </Section>
+          <Section>
+            <h3>Backend</h3>
+            <IconBox>
+              {skills
+                .filter((skill) => skill.type === "backend")
+                .map((skill) => (
+                  <Icon key={skill.name}>
+                    <img src={urlFor(skill.icon)} alt={skill.name} />
+                    <span>{skill.name}</span>
+                  </Icon>
+                ))}
+            </IconBox>
+          </Section>
+        </Main>
+        <Main>
+          <Section>
+            <h3>Database</h3>
+            <IconBox>
+              {skills
+                .filter((skill) => skill.type === "database")
+                .map((skill) => (
+                  <Icon key={skill.name}>
+                    <img src={urlFor(skill.icon)} alt={skill.name} />
+                    <span>{skill.name}</span>
+                  </Icon>
+                ))}
+            </IconBox>
+          </Section>
+          <Section>
+            <h3>Tools</h3>
+            <IconBox>
+              {skills
+                .filter((skill) => skill.type === "tool")
+                .map((skill) => (
+                  <Icon key={skill.name}>
+                    <img src={urlFor(skill.icon)} alt={skill.name} />
+                    <span>{skill.name}</span>
+                  </Icon>
+                ))}
+            </IconBox>
+          </Section>
+        </Main>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Skills;
